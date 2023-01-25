e2a763f9e996281c93e550ea853b12b73a28a75b
panecatl <ypanecatl24@gmail.com>
2023-01-25 11:52:46 -0600

    completed about me section

---
+++ src/components/Home/index.js
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import './index.scss';
+
+const Home = () => {
+
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br /> I'm Que Bella Cookies and welcome to my page! 
+                
+                On this website you will be able to see previous orders,  
+                
+                inquire about new orders, 
+                
+                as well as seeing the events that are coming up!
+                
+                Some of these events will be at farmer markets, or some of them might be cookie decorating classes!</h1>
+                <Link to='/contact' className="flat-button">CONTACT ME</Link>
+            </div>
+        </div>
+    )
+}
+
+
+export default Home; 
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,17 @@
 import './index.scss';
 import Sidebar from '../Sidebar';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
     return (
-    <>
-    <Sidebar />
-    </>
+    <div className='App'>
+        <Sidebar />
+        <div className='page'>
+            <span className='tags top-tags'>Que Bella Cookies</span>
+
+            <Outlet />
+        </div>
+    </div>
     )
 }
 
