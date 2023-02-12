804226baaf86661ae043dac2f810c9944d57abcc
panecatl <ypanecatl24@gmail.com>
2023-02-12 15:41:22 -0600

    added modal

--- src/components/Cookies /index.js
+++ src/components/Cookies /index.js
@@ -1,13 +1,17 @@
-import './index.scss';
+import { useState } from 'react';
 import Modal from '../Modal';
+import './index.scss';
+
 
 const Cookies = () => {
+
+    const [showModal, setShowModal] = useState(false)
 
     return (
         <div className='container cookie-page'>
             <div className='text-zone'>
-                <button>Hello</button>
-                <Modal />
+                <button className='openModalBtn' onClick={() => {setShowModal(true)}}>Hello</button>
+                {showModal && <Modal closeModal={setShowModal}/>}
             </div>
         </div>
     )
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import ImageSlider from '../ImageSlider'
 import './index.scss';  
 
-function Modal() {
+const Modal = ({ closeModal }) => {
 
   return (
-    <div>Modal</div>
+    <div className='modalBackground'>
+      <div className='modalContainer'>
+        <button onClick={() => closeModal(false)}> X </button>
+        <div className='title'>
+          <h1>Wedding Cookies</h1>
+        </div>
+        <div className='body'>
+          <ImageSlider />
+        </div>
+      </div>
+    </div>
   )
 }
 
